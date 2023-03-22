59043df1b5db1fcd7d31dcc70b31b3b2104fd8d3
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-03-22 11:28:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Company document layout customize
+---------------------------------
+This module allow the customization of the company document layout by enabling the code editing in __Company details__ and __Report footer__ fields.
+
---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+    'name': 'Company document layout customize',
+    'author': 'Soft-integration',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'description': False,
+    'images': [],
+    'version': '1.0.1',
+    'category': 'Hidden',
+    'demo': [],
+    'depends': ['web'],
+    'data': [
+        'views/base_document_layout_views.xml'
+             ],
+    'license': 'LGPL-3',
+}
